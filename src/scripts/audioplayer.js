091d7fcdf43c0640accc85dd65f3499ce96c3895
cd-Roid <giyawe98@gmail.com>
2020-06-19 14:08:01 +0200

    Basis Funktion um audio abzuspielen und pausieren.

--- src/scripts/audioplayer.js
+++ src/scripts/audioplayer.js
@@ -1,1 +1,24 @@
 // eslint-disable-next-line import/extensions
+
+async function playAudio() {
+  // eslint-disable-next-line no-undef
+
+
+  const playButton = document.querySelectorAll('[data-playlist-item]');
+
+
+  playButton.forEach((element) => {
+    const sound = new Howl({
+      src: ['/src/audio/hansatom_-_4real.mp3'],
+    });
+    element.addEventListener('click', () => {
+      if (sound.playing()) {
+        sound.pause();
+      } else {
+        sound.play();
+      }
+    });
+  });
+}
+
+playAudio();
